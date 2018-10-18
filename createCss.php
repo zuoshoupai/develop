1c425fb3ec75ed0de1e9e621dff159abeb281d91
Webeautiful <xiongfusong@gmail.com>
2018-10-18 18:51:29 +0800

    20181018

---
+++ createCss.php
@@ -0,0 +1,290 @@
+<?php 
+if(!isset($_POST['scene'])){
+	
+}else{
+	
+	if(empty($_POST['scene']) || !in_array($_POST['scene'],array('pc','web','wechat'))){
+		$wrong = true;
+	}else{
+		$compress = false;
+		$compress_str = '';
+		if(isset($_POST['compress']) && $_POST['compress']){
+			$compress = true;
+			$compress_str='_min_'; 
+		} 
+		$fileName="zane_".$_POST['scene'].$compress_str.".css"; 
+		header("Content-type:application/octet-stream"); 
+		header("Content-Disposition:attachment;filename = ".$fileName); 
+		create_css($_POST['scene'],$compress);
+	}
+	die;
+}
+
+function create_css($scene,$compress){
+	//$compress=false;//是否压缩
+//$scene = 'pc';
+if($scene=='wechat'){
+	$extra = 'rpx';//应用场景单位
+	$times = 1;
+}elseif($scene=='web'){
+	$extra = 'rem';//应用场景单位
+	$times = 0.01;
+}elseif($scene=='pc'){
+	$extra = 'px';//应用场景单位
+	$times = 1;
+}
+$trans='';
+if(!$compress){
+	$trans="\r\n";
+} 
+echo <<<ET
+/*fl fr text-r text-l 等等*/$trans
+ET;
+echo <<<ET
+/*固定部分*/$trans
+body, h1, h2, h3, h4, h5, h6, hr, p, blockquote, dl, dt, dd, ul, ol, li, pre, form, fieldset, legend, button, input, textarea, th, td {margin:0;padding:0;}$trans
+body, button, input, select, textarea {font:12px/1.5tahoma, arial, \5b8b\4f53;}$trans
+h1, h2, h3, h4, h5, h6{font-size:100%;}$trans
+address, cite, dfn, em, var {font-style:normal;}$trans
+code, kbd, pre, samp {font-family:couriernew, courier, monospace;}$trans
+small{font-size:12px;}$trans
+ul, ol {list-style:none;}$trans
+a {text-decoration:none;}$trans
+a:hover {text-decoration:none;}$trans
+sup {vertical-align:text-top;}$trans
+sub{vertical-align:text-bottom;}$trans
+legend {color:#000;}$trans
+fieldset, img {border:0;}$trans
+button, input, select, textarea {font-size:100%;}$trans
+table {border-collapse:collapse;border-spacing:0;}$trans
+img{max-width: 100%;max-height: 100%;}$trans
+.fl{float:left;}$trans
+.fr{float:right;}$trans
+.full-width{width:100% !important }$trans
+.fly-out{position: absolute;left: -10000px;}$trans
+.text-c{text-align:center;}$trans
+.text-r{text-align:right;}$trans
+.text-l{text-align:left }$trans
+.pointer{cursor:pointer;}$trans
+.none{display:none;}$trans
+.hidden{visibility: hidden;}$trans
+.clear{clear:both;}$trans
+.position-a{position:absolute }$trans
+.position-r{position:relative }$trans
+.bold{font-weight:600}$trans
+.padding0{padding:0}$trans
+.padding-r0{padding-right:0}$trans
+.padding-l0{padding-left:0}$trans
+.padding-t0{padding-top:0}$trans
+.padding-b0{padding-bottom:0}$trans
+.margin0{margin:0;}$trans
+.margin-r0{margin-right:0}$trans
+.margin-l0{margin-left:0}$trans
+.margin-t0{margin-top:0}$trans
+.margin-b0{margin-bottom:0}$trans
+.border0{border:0;}$trans
+.border-r0{border-right:0}$trans
+.border-l0{border-left:0}$trans
+.border-t0{border-top:0}$trans
+.border-b0{border-bottom:0}$trans
+.block{display: block}$trans
+.inline-block{display: inline-block}$trans
+.index9{z-index:9}$trans
+.index99{z-index:99}$trans
+.index999{z-index:999}$trans
+.index9999{z-index:9999}$trans
+.width25{width:25%}$trans
+.width50{width:50%}$trans
+.width75{width:75%}$trans
+.width100{width:100%}$trans
+.lights:hover{color:#666;}$trans
+	
+ET;
+if($scene=='pc'){
+echo <<<ET
+@media(min-width:1200px){ $trans
+.z-container{width:1200px;margin:0 auto;}
+}/$trans
+ET;
+}
+echo "/*枚举部分-字号*/$trans"; 
+$font_s=5;
+$font_e=40;
+for($i=$font_s;$i<=$font_e;$i++){
+	echo ".font".$i."{font-size:".$i*$times.$extra.";}$trans";
+}
+echo "/*枚举部分-外边距*/$trans";  
+$s=5;
+$step=5;
+$e=100;
+for($i=$s;$i<=$e;$i=$i+$step){
+	echo ".margin".$i."{margin:".$i*$times.$extra.";}$trans";
+	echo ".margin-t".$i."{margin-top:".$i*$times.$extra.";}$trans";
+	echo ".margin-r".$i."{margin-right:".$i*$times.$extra.";}$trans";
+	echo ".margin-b".$i."{margin-bottom:".$i*$times.$extra.";}$trans";
+	echo ".margin-l".$i."{margin-left:".$i*$times.$extra.";}$trans"; 
+}
+echo "/*枚举部分-内边距*/$trans";  
+$s=5;
+$step=5;
+$e=100;
+for($i=$s;$i<=$e;$i=$i+$step){
+	echo ".padding".$i."{padding:".$i*$times.$extra.";}$trans";
+	echo ".padding-t".$i."{padding-top:".$i*$times.$extra.";}$trans";
+	echo ".padding-r".$i."{padding-right:".$i*$times.$extra.";}$trans";
+	echo ".padding-b".$i."{padding-bottom:".$i*$times.$extra.";}$trans";
+	echo ".padding-l".$i."{padding-left:".$i*$times.$extra.";}$trans"; 
+	echo ".side".$i."{padding-left:".$i*$times.$extra.";padding-right:".$i*$times.$extra.";}$trans";  
+}
+echo "/*枚举部分-倒角*/$trans";  
+$s=5;
+$step=5;
+$e=100;
+for($i=$s;$i<=$e;$i=$i+$step){
+	echo ".border-radius".$i."{border-radius:".$i*$times.$extra.";}$trans";
+} 
+echo "/*枚举部分-绝对定位*/$trans";  
+$s=2;
+$step=2;
+$e=100;
+for($i=$s;$i<=$e;$i=$i+$step){
+	echo ".top".$i."{top:".$i*$times.$extra.";}$trans";
+	echo ".right".$i."{right:".$i*$times.$extra.";}$trans";
+	echo ".bottom".$i."{bottom:".$i*$times.$extra.";}$trans";
+	echo ".left".$i."{left:".$i*$times.$extra.";}$trans";
+} 
+if($scene=='pc'){
+	$times = 0.8;
+	echo <<<ET
+}$trans
+@media(max-width:1199px){ $trans
+.z-container{width:960px;margin:0 auto;}
+}/$trans
+ET;
+	echo "/*枚举部分-字号*/$trans"; 
+	$font_s=5;
+	$font_e=40;
+	for($i=$font_s;$i<=$font_e;$i++){
+		echo ".font".$i."{font-size:".$i*$times.$extra.";}$trans";
+	}
+	echo "/*枚举部分-外边距*/$trans";  
+	$s=5;
+	$step=5;
+	$e=100;
+	for($i=$s;$i<=$e;$i=$i+$step){
+		echo ".margin".$i."{margin:".$i*$times.$extra.";}$trans";
+		echo ".margin-t".$i."{margin-top:".$i*$times.$extra.";}$trans";
+		echo ".margin-r".$i."{margin-right:".$i*$times.$extra.";}$trans";
+		echo ".margin-b".$i."{margin-bottom:".$i*$times.$extra.";}$trans";
+		echo ".margin-l".$i."{margin-left:".$i*$times.$extra.";}$trans"; 
+	}
+	echo "/*枚举部分-内边距*/$trans";  
+	$s=5;
+	$step=5;
+	$e=100;
+	for($i=$s;$i<=$e;$i=$i+$step){
+		echo ".padding".$i."{padding:".$i*$times.$extra.";}$trans";
+		echo ".padding-t".$i."{padding-top:".$i*$times.$extra.";}$trans";
+		echo ".padding-r".$i."{padding-right:".$i*$times.$extra.";}$trans";
+		echo ".padding-b".$i."{padding-bottom:".$i*$times.$extra.";}$trans";
+		echo ".padding-l".$i."{padding-left:".$i*$times.$extra.";}$trans"; 
+		echo ".side".$i."{padding-left:".$i*$times.$extra.";padding-right:".$i*$times.$extra.";}$trans";  
+	}
+	echo "/*枚举部分-倒角*/$trans";  
+	$s=5;
+	$step=5;
+	$e=100;
+	for($i=$s;$i<=$e;$i=$i+$step){
+		echo ".border-radius".$i."{border-radius:".$i*$times.$extra.";}$trans";
+	} 
+	echo "/*枚举部分-绝对定位*/$trans";  
+	$s=2;
+	$step=2;
+	$e=100;
+	for($i=$s;$i<=$e;$i=$i+$step){
+		echo ".top".$i."{top:".$i*$times.$extra.";}$trans";
+		echo ".right".$i."{right:".$i*$times.$extra.";}$trans";
+		echo ".bottom".$i."{bottom:".$i*$times.$extra.";}$trans";
+		echo ".left".$i."{left:".$i*$times.$extra.";}$trans";
+	} 
+	echo "}$trans";
+}
+echo <<<EOD
+/*弹性布局*/$trans
+.flex{display:flex;}$trans
+.flex-row{flex-direction: row}$trans
+.flex-column{flex-direction:column}$trans
+.flex-start{justify-content: flex-start}$trans
+.flex-end{justify-content:flex-end}$trans
+.flex-cente{justify-content:center}$trans
+.flex-space-between{justify-content: space-between}$trans
+.flex-space-around{space-around;}$trans
+.flex-align-center{align-items:center;}$trans
+$trans/* 拓展设置  需要自定义*/$trans
+.color0{color:#000;}$trans
+.color3{color:#333;}$trans
+.color6{color:#666;}$trans
+.color9{color:#999;}$trans
+.color-b{color:#50d7da;}$trans
+.color-w{color:#fff;}$trans 
+.bg-w{background-color:#fff;}$trans
+EOD;
+echo "/*end*/";
+
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<meta name="viewport" content="width=device-width">
+<title>webpage crawler</title>
+<style>
+	h4{text-align:center}
+	.contain{width: 99%;margin: 20px auto;max-width: 700px;}
+	div,form{font-size:20px;}
+	form{width:99%;margin:0 auto}
+	form  select,form input{width:100%;height: 40px;margin:0;padding:0}
+	.group{margin-bottom:20px;}
+	.form-group{margin-bottom: 10px;overflow: hidden;font-size: 15px;}
+	.form-group label{width:160px;display: block;float: left;}
+	.form-group input{width: 180px;float: left;height: 20px;}
+	.web_down{font-size:15px;.height: 20px;line-height: 20px;}
+	.web_down input{width:15px;height: 20px;float:left}
+	.web_down span{height:20px;line-height:20px;}
+</style>
+</head>
+<body>
+<div>
+	<h4>webpage BaseStyle file</h4>
+	<div class="contain">
+		<form method="post">
+			<div style="border-right:4px solid #fff;">
+				<select name="scene" >
+					<option value="">please select one
+					<option value="pc">pc
+					<option value="web">web
+					<option value="wechat">wechat
+				</select>
+			</div>
+			<input class="group" type="submit" value="submit"/>
+			 <fieldset>
+				<legend>default config</legend>
+				<label for="web_down" class="web_down">
+					<input type="checkbox" id="web_down" name="compress"  /><span>compress</span>
+				</label>
+			</fieldset>
+			<?php
+		if(isset($first)&&$first==1){
+			echo "<div style=\"text-align: center; margin-top: 20px;}\"><a href=".$download.">下载地址</div>";
+		}
+		if(isset($wrong)&& $wrong){
+			echo "<h5 style='color:red'>Please fill in the correct information!</h5>";
+		}
+		?>
+		</form>
+		
+	</div>
+</div>
+
+
+</body>
+</html> 
---
+++ phpCraw.php
@@ -0,0 +1,475 @@
+<?php 
+class craw{
+	private $root_url;
+	private $root_dir ;
+	private $root_path;
+	private $host_path;
+	private $root_http = 'http';
+	public $_js = 'js';
+	public $_css = 'css';
+	public $_image = 'images';
+	public $_picture = 'picture';
+	public $_web_down = false;
+	public function __construct($root_url){
+		$this->root_url = $root_url;
+		$fileinfo = pathinfo($root_url);
+		$this->root_path = $fileinfo['dirname']; //网站文件目录
+		
+		$url_info = explode('/',$root_url);
+		if(strpos($url_info[0],'https')!==false){
+			$this->root_http = 'https';
+		}
+		$this->host_path = $url_info[0].'//'.$url_info[2];
+		$this->root_dir = time();
+	}
+	public function run(){
+		
+		$data = $this->parseDom($this->root_url); 
+		$this->downloadFile($data['js'],1);
+		//$this->downloadFile($data['css'],2);
+		$this->downloadFile($data['img'],3);  
+		//解析css图片并下截
+		$this->parseCssFile($data['css']); 
+		
+		$content = $this->parseloadFile($data['js'],1,$data['content']);
+		$content = $this->parseloadFile($data['css'],2,$content);
+		$content = $this->parseloadFile($data['img'],3,$content);
+		$content = $this->extra_parse($content); 
+		//写入index.html
+		$file_dir = $this->root_dir;
+		if(!is_dir($file_dir)){
+			mkdir($file_dir,0777,true);
+			chmod($file_dir,0777); 
+		}
+		file_put_contents($file_dir.'/index.html',$content);
+		return $this->creatzip();
+		
+	}
+	//其它匹配规则
+	private function extra_parse($content){ 
+		$root_http = $this->root_http;
+		$rule_ex = "/img[\'\"]?\:[\'\"]?\/\/(.*?)[\'\"]/"; 
+		$replace_ex = "img':'".$root_http."://\${1}'";  
+		$content =  Preg_replace($rule_ex,$replace_ex,$content);  
+		//$rule_ex = "/image[\'\"]?\:[\'\"]?[\\]?\/[\\]?\/(.*?)[\'\"]/"; 
+		//$replace_ex = "image\":\"".$root_http.":\${1}";  
+		//$content =  Preg_replace($rule_ex,$replace_ex,$content); 
+		return $content;
+	}
+	private function parseDom($root_url){
+  
+		$data =  $this->_get_contents($root_url,2);
+		$encoding = mb_detect_encoding($data, array("ASCII","UTF-8","GB2312","GBK","BIG5")); 
+		$keytitle = iconv($encoding,"UTF-8",$data); 
+		//js  <script type="text/javascript" src="js/artTemplate_d6facd8.js"></script>
+		$rule_js = "/<[\s]*script[^>]+src=[\s]*[\'\"](.*?)\.js/";
+		preg_match_all($rule_js,$data,$result_js); 
+		$js_arr = $result_js[1]; 
+		//css
+		$rule_css = "/<[\s]*link[^>]+href=[\s]*[\'\"](.*?)\.css/";
+		preg_match_all($rule_css,$data,$result_css); 
+		$css_arr = $result_css[1];
+		
+		//image
+		$rule_image = "/<[\s]*img[^>]+src=[\s]{0,4}[\'\"](.*?)[\'\"]/";
+		preg_match_all($rule_image,$data,$result_image);
+		$img_arr = $this->_filter_array($result_image[1]);  
+		
+		$datas = array();
+		$datas['js'] = $js_arr;
+		$datas['css'] = $css_arr;
+		$datas['img'] = $img_arr;
+		$datas['content'] = $data;
+		return $datas;
+	}
+	/**
+	*** 解析并下载css中的图片
+	*** $url_arr 文件数组，不带拓展名
+	*** $type 文件类型 1：js 2: css 3: image
+	*/
+	private function parseCssFile($url_arr)
+	{ 
+		$host_path = $this->host_path;
+		$root_path = $this->root_path;
+		$Dir = $this->root_dir;
+		$root_http = $this->root_http;
+		$extension_name = '.css'; 
+		$extension_pic = ''; 
+		$file_dir = $Dir.'/'.$this->_picture;
+		$file_css = $Dir.'/'.$this->_css;
+		$file_picture = '../'.$this->_picture;
+		if(!is_dir($file_dir)){
+			mkdir($file_dir,0777,true);
+			chmod($file_dir,0777); 
+		}
+		foreach($url_arr as $url)
+		{
+			
+			if(strpos($url,'http')!==false){
+				$file_target = $url.$extension_name;
+			}elseif(substr($url,0,2)=='//'){
+				$file_target = $root_http.':'.$url.$extension_name;
+			}elseif(substr($url,0,1)=='/'){
+				$file_target = $host_path.$url.$extension_name;
+			}else{
+				
+				$file_target = $root_path.'/'.$url.$extension_name;
+			}
+			$fileinfo = pathinfo($url);
+			$fileName = $fileinfo['basename'];
+			$file = $this->_get_contents($file_target); 
+			$rule_images = "/[\s]*background(-image)?[\s]*:[\s]*url\([\'\"]?(.*?)[\'\"]?\)/";
+			preg_match_all($rule_images,$file,$result_images);
+			$data_images_arr = $this->_filter_array($result_images[2]);   
+			$max_count=20;
+			$j=0;
+			foreach($data_images_arr as $vv){
+				if($j>$max_count){
+					break;
+				}
+				if(strpos($vv,'data:image')!==false){
+					continue;
+				}
+				if(strpos($vv,'http')!==false){
+					$file_target1 = $vv;
+				}elseif(substr($vv,0,2)=='//'){
+					$file_target1 = $root_http.':'.$vv;
+				}elseif(substr($vv,0,1)=='/'){
+					$file_target1 = $host_path.$vv;
+				}else{
+					$file_target1 = dirname($file_target).'/'.$vv;
+				}  
+				$fileinfo1 = pathinfo($vv);
+				$fileName1 = $fileinfo1['basename'];
+				try{
+					$file1 = $this->_get_contents($file_target1); 
+					file_put_contents($file_dir.'/'.$fileName1,$file1); 
+					
+				}catch(Exception $e){
+					//echo $e->getMessage();
+				}  
+				$file = str_replace($vv,$file_picture.'/'.$fileName1,$file);
+				$j++;
+			}   
+			if(!is_dir($file_css)){
+				mkdir($file_css,0777,true);
+				chmod($file_dir,0777); 
+			}
+			file_put_contents($file_css.'/'.$fileName.$extension_name,$file);
+			 
+		}  
+	}
+	private function _filter_array($arr){ 
+		if(!is_array($arr)){
+			return $arr;
+		}
+		$arr_new = array();
+		foreach($arr as $v){
+			if(empty($v)){
+				continue;
+			} 
+			if(strpos($v,'#')!==false){
+				continue;
+			}
+			if(strpos($v,'{')!==false){
+				continue;
+			}
+			if(strpos($v,'}')!==false){
+				continue;
+			}
+			if(strpos($v,'?')!==false){
+				$v=substr($v,0,strpos($v,'?'));
+			}
+			$arr_new[] = $v;
+		}
+		return $arr_new;
+	}
+	/**
+	*** 获取远程文件内容
+	*** $url_arr 文件地址
+	*** $op 可选参数，2表示强制curl获取
+	*/
+	private function _get_contents($root_url,$op=1){ 
+		 
+		if(strpos($root_url,'https')!==false || $op==2){
+			$ch = curl_init();//初始化一个资源
+			$headers = array(); 
+			$headers[] = 'User-Agent:Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11';
+			$headers[]='X-FORWARDED-FOR:111.222.333.9';
+			$headers[] = 'CLIENT-IP:111.222.333.9';
+			curl_setopt($ch,CURLOPT_URL,$root_url);//设置我们要获取的网页
+			curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);//关闭直接输出
+			curl_setopt($ch, CURLOPT_HEADER, 0);
+			curl_setopt($ch, CURLOPT_HTTPHEADER  , $headers);  
+			curl_setopt( $ch , CURLOPT_SSL_VERIFYPEER, false); //关闭ssl
+			curl_setopt($ch, CURLOPT_REFERER, "http://www.test.com");   
+			$data = curl_exec($ch);
+			curl_close($ch);
+			return $data;
+		}else{
+			return	file_get_contents($root_url); 
+			
+		}
+	}
+	/**
+	*** 下载文件函数
+	*** $url_arr 文件数组，不带拓展名
+	*** $type 文件类型 1：js 2: css 3: image
+	*/
+	private function downloadFile($url_arr,$type)
+	{
+		$host_path = $this->host_path;
+		$root_path = $this->root_path;
+		$root_http = $this->root_http;
+		$Dir = $this->root_dir; 
+		$max_count=100;
+		$op_true = false;
+		if($type == 1){
+			$extension_name = '.js';
+			$file_dir = $Dir.'/'.$this->_js;
+			$op_true=true;
+		}elseif($type==2){
+			$extension_name = '.css';
+			$file_dir = $Dir.'/'.$this->_css;
+			$op_true=true;
+		}elseif($type ==3){
+			$extension_name = '';
+			$file_dir = $Dir.'/'.$this->_image;;
+			$max_count=100;
+		} 
+		$j=0;
+		foreach($url_arr as $url)
+		{ 
+			$op = false;
+			if($j>$max_count){
+				break;
+			}
+			if(strpos($url,'http')!==false){
+				$file_target = $url.$extension_name;
+				$op = false;
+			}elseif(substr($url,0,2)=='//'){
+				$file_target =$root_http.':'.$url.$extension_name;
+				$op = false;
+			}elseif(substr($url,0,1)=='/'){
+				$file_target = $host_path.$url.$extension_name;
+			}else{ 
+				$file_target = $host_path.'/'.$url.$extension_name;
+			}
+			if($this->_web_down || $op || $op_true){  
+				$fileinfo = pathinfo($file_target);
+				$fileName = $fileinfo['basename'];   
+				$file = $this->_get_contents($file_target); 
+				if(!is_dir($file_dir)){
+					mkdir($file_dir,0777,true);
+					chmod($file_dir,0777); 
+				} 
+				file_put_contents($file_dir.'/'.$fileName,$file);
+			//return $file_dir.'/'.$fileName; 
+				$j++;
+			} 
+		} 
+	}
+	/**
+	*** 解析文件函数
+	*** $url_arr 文件数组，不带拓展名
+	*** $type 文件类型 1：js 2: css 3: image
+	*** 解析文件内容
+	*/
+	private function parseloadFile($url_arr,$type,$content)
+	{
+		
+		$host_path = $this->host_path;
+		$root_path = $this->root_path;
+		$Dir = $this->root_dir;
+		$root_http = $this->root_http;
+		$op_true = false;
+		if($type == 1){
+			$extension_name = '.js';
+			$file_dir = $this->_js;
+			$op_true  = true;
+		}elseif($type==2){
+			$extension_name = '.css';
+			$file_dir = $this->_css;
+			$op_true  = true;
+		}elseif($type ==3){
+			$extension_name = '';
+			$file_dir = $this->_image;
+		}
+		foreach($url_arr as $url)
+		{
+			$op = false;
+			if(strpos($url,'http')!==false){
+				$file_target = $url.$extension_name;
+				$op = false;
+				$front_web='';
+			}elseif(substr($url,0,2)=='//'){
+				$file_target =$root_http.':'.$url.$extension_name;
+				$op = false;
+				$front_web=$root_http.':';
+			}else{
+				$front_web=$root_path;
+			}
+			if($this->_web_down || $op || $op_true){ 
+				$fileinfo = pathinfo($url.$extension_name);
+				$fileName = $fileinfo['basename'];
+				$content = str_replace($url.$extension_name,$file_dir.'/'.$fileName,$content);
+			}elseif($type ==3 && !$op){
+				$content = str_replace($url.$extension_name,$front_web.$url.$extension_name,$content);
+			}
+		}
+		return $content;
+	}
+	private function creatzip()
+	{
+		$Dir = $this->root_dir;
+		$files = $this->list_dir($Dir.'/');
+		$filename = $Dir.".zip"; //最终生成的文件名（含路径）   
+		if(!file_exists($filename)){   
+		//重新生成文件   
+			$zip = new ZipArchive();//使用本类，linux需开启zlib，windows需取消php_zip.dll前的注释   
+			if ($zip->open($filename, ZIPARCHIVE::CREATE)!==TRUE) 
+			{   
+				exit('无法打开文件，或者文件创建失败');
+			}   
+			foreach( $files as $val){   
+				if(file_exists($val)){   
+					$zip->addFile( $val, str_replace($Dir.'/','',$val));//第二个参数是放在压缩包中的文件名称，如果文件可能会有重复，就需要注意一下   
+				}   
+			}   
+			$zip->close();//关闭
+		}
+		return $filename;
+	}
+	//获取文件列表
+	private function list_dir($dir){
+			$result = array();
+			if (is_dir($dir)){
+				$file_dir = scandir($dir);
+				foreach($file_dir as $file){
+					if ($file == '.' || $file == '..'){
+						continue;
+					}
+					elseif (is_dir($dir.$file)){
+						$result = array_merge($result, $this->list_dir($dir.$file.'/'));
+					}
+					else{
+						array_push($result, $dir.$file);
+					}
+				}
+			}
+			return $result;
+	}	
+   
+}   
+ini_set('max_execution_time', '200');
+if(isset($_POST['url'])){
+	$data['_css'] = $_POST['css'];
+	$data['_js'] = $_POST['js'];
+	$data['_image'] = $_POST['image'];
+	$data['_picture'] = $_POST['picture'];
+	
+	$wrong = false;
+	
+	foreach($data as $k=>$v){
+		if(preg_match("/[\'.,:;*?~`!@#$%^&+=)(<>{}]|\]|\[|\/|\\\|\"|\|/",$v)){   
+			$wrong=true;
+		}
+		if(empty(trim($v))){   
+			$wrong=true;
+		}
+		
+	}
+	if(empty(trim($_POST['url']))){   
+		$wrong=true;
+	} 
+	if(!preg_match('/^((http|ftp|https):\/\/)[\w-_.]+(\/[\w-_]+)*\/?$/',trim($_POST['url']))){
+		$wrong=true; 
+	} 
+	if(!$wrong){
+		$craw = new craw(trim($_POST['url']));
+		
+		foreach($data as $k=>$v){
+			$craw->$k = $v;
+		}
+		if(isset($_POST['web_down']) && $_POST['web_down']){
+			$craw->_web_down=true;
+		}   
+		$download = $craw->run();
+		header('Location: ?file='.$download);
+	}else{
+		unset($_GET['file']);
+	}
+}
+if(isset($_GET['file'])){
+	$download = $_GET['file'];
+	if(file_exists($download)){
+		$first=1;
+	}
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<meta name="viewport" content="width=device-width">
+<title>webpage crawler</title>
+<style>
+	h4{text-align:center}
+	.contain{width: 99%;margin: 20px auto;max-width: 700px;}
+	div,form{font-size:20px;}
+	form{width:99%;margin:0 auto}
+	form  input{width:100%;height: 40px;margin:0;padding:0}
+	.group{margin-bottom:20px;}
+	.form-group{margin-bottom: 10px;overflow: hidden;font-size: 15px;}
+	.form-group label{width:160px;display: block;float: left;}
+	.form-group input{width: 180px;float: left;height: 20px;}
+	.web_down{font-size:15px;.height: 20px;line-height: 20px;}
+	.web_down input{width:15px;height: 20px;float:left}
+	.web_down span{height:20px;line-height:20px;}
+</style>
+</head>
+<body>
+<div>
+	<h4>webpage crawler</h4>
+	<div class="contain">
+		<form method="post">
+			<div style="border-right:4px solid #fff;">
+				<input name="url" type="text" placeholder="please input your the website" />
+			</div>
+			<input class="group" type="submit" value="submit"/>
+			 <fieldset>
+				<legend>default config</legend>
+				<div class="form-group">
+					<label>css Dir</label>
+					<input name="css" type="text" placeholder="" value="css"/>
+				</div>
+				<div class="form-group">
+					<label>js Dir</label>
+					<input name="js" type="text" placeholder="" value="js"/>
+				</div>
+				<div class="form-group">
+					<label>image Dir</label>
+					<input name="image" type="text" placeholder="" value="image"/>
+				</div>
+				<div class="form-group">
+					<label>css background Dir</label>
+					<input name="picture" type="text" placeholder="" value="picture"/>
+				</div>
+				<label for="web_down" class="web_down">
+					<input type="checkbox" id="web_down" name="web_down"  /><span>download web image</span>
+				</label>
+			</fieldset>
+			<?php
+		if(isset($first)&&$first==1){
+			echo "<div style=\"text-align: center; margin-top: 20px;}\"><a href=".$download.">下载地址</div>";
+		}
+		if(isset($wrong)&& $wrong){
+			echo "<h5 style='color:red'>Please fill in the correct information!</h5>";
+		}
+		?>
+		</form>
+		
+	</div>
+</div>
+
+
+</body>
+</html> 
