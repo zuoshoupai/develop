b889567d408f75dbc1aa1d90ea913d903a8f244f
lize <15373850656@163.com>
2019-05-14 10:33:40 +0800

    5142

--- Readme.md
+++ Readme.md
@@ -1,12 +1,12 @@
 # 个人开发的实用工具
 ### 一. **仿站工具(phpCraw.php)**
-作用：抓取网站页面的前端代码包括图片，js,css文件，实例代码 [https://api.00iy.com/phpCraw.php](https://api.00iy.com/phpCraw.php)<br/>
+作用：抓取网站页面的前端代码包括图片，js,css文件，实例代码 [http://develop.aajiao.cn/phpCraw.php](https://develop.aajiao.cn/phpCraw.php)<br/>
 1. 可设置图片，js,css存储目录
 2. 可匹配抓取背景图片，base64编码图片
 3. 可设置是否下载网络图片
     
 ### 二. **生成基础style.css工具(createCss.php)**
-作用：生成前端基本的边距，颜色等类 ，实例代码 [https://api.00iy.com/createCss.php](https://api.00iy.com/createCss.php)<br/>
+作用：生成前端基本的边距，颜色等类 ，实例代码 [http://develop.aajiao.cn/createCss.php](http://develop.aajiao.cn/createCss.php)<br/>
 
 1. 根据pc,web,微信小程序不同场景生成文件
 2. 可设置是否压缩
@@ -19,20 +19,6 @@
 使用：按下空格或,自动间隔，取值格式为 string,string,string,后期版本可自定义<br>
 
 示意代码：
-```css
-<style>
-.zane-spaceme{position: relative}
-.zane-spaceme-choices{overflow:hidden;list-style:none;z-index: 10;padding: 0}
-.zane-spaceme-choices .search-choice{float:left;position:relative;position: relative;padding: 2px 16px;margin-right: 10px;background-color:#009688;color: #fff;margin-bottom: 10px;    border-radius: 10px;}
-.zane-spaceme-choices .search-choice-close{position: absolute;color: #f13d3d;top: -6px;right: 0px;font-weight: bold}
-.zane-spaceme-choices .search-field{float:left}
-.zane-spaceme-choices .search-field input {width: 70px;margin-bottom: 10px;}
-.normal .search-choice{background-color:#1E9FFF;color: #fff;}
-.warm .search-choice{background-color:#FFB800;color: #fff;}
-.danger .search-choice{background-color:#FF5722;color: #fff;}
-.danger .zane-spaceme-choices .search-choice-close{color: #666;} 
-</style>
-```
 ```javascript
 <script>
 $(function(){  
--- jquery_spaceme.js
+++ jquery_spaceme.js
@@ -1,66 +1,65 @@
 ;(function ($) {
-      $.fn.spaceme = function (options) { //定义插件方法名
-            options = $.extend({
-              id: '',
-              skin:'blue'
-              },options); 
-			var old_str = this.val(),old_str_arr = old_str.split(','),old_width=this.css('width'),old_paading=this.parent().css('padding-right'),id=options.id,skin=options.skin;
-           if(id.replace(/\s/g,'').length>0){
-           		id = 'id="'+id.replace(/\s/g,'')+'"';
-           }
-           if(skin.replace(/\s/g,'').length>0){
-           		skin = skin.replace(/\s/g,'')+'"';;
-           }
-           var container = '<div '+id+' class="zane-spaceme zane-spaceme-multi '+skin+'" ><ul class="zane-spaceme-choices" >';
-           for(var i=0;i<old_str_arr.length;i++)
-           {
-			    if (old_str_arr[i].replace(/(^s*)|(s*$)/g, "").length >0) 
-				{ 
-					container+='<li class="search-choice" ><span>'+old_str_arr[i]+'</span><a class="search-choice-close">Χ</a></li>'; 
-				} 
-           	}
-           container+='<li class="search-field"><input  type="text"></li></ul></div>';
-		   this.css({'position': 'absolute','top': '0','z-index': '1','width':(old_width-15)+'px','text-indent':'999999999999px'}); 
-		   this.focus(function(){
-				$(".zane-spaceme .search-field input").focus();
-		   })
-           this.before(container);
-           var obj = this;
-           this.hide();
-           	$(".zane-spaceme:not(.search-choice)").click(function(){
-           		$(this).find("input").focus();
-           	});
-            $(".zane-spaceme input").keydown(function(e){
-			    if(!e) var e = window.event; 
-			    if(e.keyCode==32||e.keyCode==188){  
-			         var str = $(this).val(); 
-					 $(this).val('  ');
-			         if(/^ *$/.test(str)!=true)
-			         {
-						str = str.replace(/(^\s*)/g,'');
-			         	var html = '<li class="search-choice"><span>'+str+'</span><a class="search-choice-close">Χ</a></li>';
-				         $(this).parent().before(html);
-				         
-			         }  
-					 
-			        var consume = '';
-			        $(this).parent().prevAll().children("span").each(function(){
-			        	consume+=$(this).text()+',';
-					});  
-					obj.val(consume.replace(/(,$)/g,'')); 
-					
-			    }
-			 })
-            $(".zane-spaceme").on('click','.search-choice-close',function(){
-           		obj_par = $(this).parent().parent();
-            	$(this).parent().remove(); 
-            	var consume = '';   
-            	obj_par.find("span").each(function(){
-			        	consume+=$(this).text()+',';
-				}); 
+    $.fn.spaceme = function (options) { //定义插件方法名
+		options = $.extend({
+		id: '',
+		skin:'blue'
+		  },options); 
+		var old_str = this.val(),old_str_arr = old_str.split(','),old_width=this.css('width'),old_paading=this.parent().css('padding-right'),id=options.id,skin=options.skin;
+	   if(id.replace(/\s/g,'').length>0){
+			id = 'id="'+id.replace(/\s/g,'')+'"';
+	   }
+	   if(skin.replace(/\s/g,'').length>0){
+			skin = skin.replace(/\s/g,'')+'"';;
+	   }
+	   var container = '<div '+id+' class="zane-spaceme zane-spaceme-multi '+skin+'" ><ul class="zane-spaceme-choices" >';
+	   for(var i=0;i<old_str_arr.length;i++)
+	   {
+			if (old_str_arr[i].replace(/(^s*)|(s*$)/g, "").length >0) 
+			{ 
+				container+='<li class="search-choice" ><span>'+old_str_arr[i]+'</span><a class="search-choice-close">Χ</a></li>'; 
+			} 
+		}
+	   container+='<li class="search-field"><input  type="text"></li></ul></div>';
+	   this.css({'position': 'absolute','top': '0','z-index': '1','width':(old_width-15)+'px','text-indent':'999999999999px'}); 
+	   this.focus(function(){
+			$(".zane-spaceme .search-field input").focus();
+	   })
+	   this.before(container);
+	   var obj = this;
+	   this.hide();
+		$(".zane-spaceme:not(.search-choice)").click(function(){
+			$(this).find("input").focus();
+		});
+		$(".zane-spaceme input").keydown(function(e){
+			if(!e) var e = window.event; 
+			if(e.keyCode==32||e.keyCode==188){  
+				 var str = $(this).val(); 
+				 $(this).val('  ');
+				 if(/^ *$/.test(str)!=true)
+				 {
+					str = str.replace(/(^\s*)/g,'');
+					var html = '<li class="search-choice"><span>'+str+'</span><a class="search-choice-close">Χ</a></li>';
+					 $(this).parent().before(html);
+				}  
+				var consume = '';
+				$(this).parent().prevAll().children("span").each(function(){
+					consume+=$(this).text()+',';
+				});  
 				obj.val(consume.replace(/(,$)/g,'')); 
-            	return false;
-            });
-            return this;
-        }
+			}
+		 })
+		$(".zane-spaceme").on('click','.search-choice-close',function(){
+			obj_par = $(this).parent().parent();
+			$(this).parent().remove(); 
+			var consume = '';   
+			obj_par.find("span").each(function(){
+					consume+=$(this).text()+',';
+			}); 
+			obj.val(consume.replace(/(,$)/g,'')); 
+			return false;
+		});
+		var space_css='<style>.zane-spaceme{position: relative;border: 1px solid #eee;padding: 5px 15px;border-radius: 20px;}.zane-spaceme-choices{overflow:hidden;list-style:none;z-index: 10;padding: 0;margin: 0;display: flex;align-items: center;}.zane-spaceme-choices .search-choice{float:left;position:relative;position: relative;padding: 2px 16px;margin-right: 10px;background-color:#009688;color: #fff;border-radius: 10px;}.zane-spaceme-choices .search-choice-close{position: absolute;color: #f13d3d;top: -6px;right: 0px;font-weight: bold}.zane-spaceme-choices .search-field{float:left}.zane-spaceme-choices .search-field input {width: 70px;}.normal .search-choice{background-color:#1E9FFF;color: #fff;}.warm .search-choice{background-color:#FFB800;color: #fff;}.danger .search-choice{background-color:#FF5722;color: #fff;}.danger .zane-spaceme-choices .search-choice-close{color: #666;}</style>';
+	   $('head').append(space_css);
+		return this;
+	}
 })(window.jQuery);
