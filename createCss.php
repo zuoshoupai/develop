613f075ce092ca90ae641f8ac5d7eacd0899cb8b
lize <15373850656@163.com>
2019-06-03 09:27:59 +0800

    20190603

--- createCss.php
+++ createCss.php
@@ -1,7 +1,7 @@
 <?php 
 /**
 describe:去掉绝对定位的枚举，节省空间；类名向bootstrap靠拢，方便过渡
-date:2019-04-24
+date:2019-06-03
 author:zane
 **/
 if(!isset($_POST['scene'])){
@@ -44,33 +44,33 @@ function create_css($scene,$compress){
 	$trans='';
 if($scene=='pc'){
 echo <<<ET
-.container-fluid{width:100%;}$trans
-.z-container{width:1200px;margin:0 auto;min-width:1300px;}
+.container-fluid{width:100% !important;}$trans
+.z-container{width:1200px;margin:0 auto;min-width:1300px !important;}
 ET;
 }
 echo "/*弹性布局*/$trans
-.border-box{box-sizing: border-box;}
-.d-none{display:none;}$trans
+.border-box{box-sizing: border-box !important;}
+.d-none{display:none !important;}$trans
 .d-block{display: block}$trans
-.d-inline-block{display: inline-block}$trans
-.d-flex{display:flex;}$trans
-.flex-row{flex-direction: row}$trans
-.flex-column{flex-direction:column}$trans
-.justify-content-start{justify-content: flex-start}$trans
-.justify-content-end{justify-content:flex-end}$trans
-.justify-content-center{justify-content:center}$trans
-.justify-content-between{justify-content: space-between}$trans
-.justify-content-around{justify-content: space-around;}$trans
-.align-items-center{align-items:center;}$trans
-.flex-wrap{flex-wrap: wrap;}$trans
+.d-inline-block{display: inline-block !important}$trans
+.d-flex{display:flex !important;}$trans
+.flex-row{flex-direction: row !important}$trans
+.flex-column{flex-direction:column !important}$trans
+.justify-content-start{justify-content: flex-start !important}$trans
+.justify-content-end{justify-content:flex-end !important}$trans
+.justify-content-center{justify-content:center !important}$trans
+.justify-content-between{justify-content: space-between !important}$trans
+.justify-content-around{justify-content: space-around !important;}$trans
+.align-items-center{align-items:center !important;}$trans
+.flex-wrap{flex-wrap: wrap !important;}$trans
 $trans/* 拓展设置  需要自定义*/$trans
-.color0{color:#000;}$trans
-.color3{color:#333;}$trans
-.color6{color:#666;}$trans
-.color9{color:#999;}$trans
-.color-b{color:#50d7da;}$trans
-.color-w{color:#fff;}$trans 
-.bg-w{background-color:#fff;}$trans
+.color0{color:#000 !important;}$trans
+.color3{color:#333 !important;}$trans
+.color6{color:#666 !important;}$trans
+.color9{color:#999 !important;}$trans
+.color-b{color:#50d7da !important;}$trans
+.color-w{color:#fff !important;}$trans 
+.bg-w{background-color:#fff !important;}$trans
 ";
 
 echo "
@@ -91,61 +91,61 @@ fieldset, img {border:0;}$trans
 button, input, select, textarea {font-size:100%;}$trans
 table {border-collapse:collapse;border-spacing:0;}$trans
 img{max-width: 100%;max-height: 100%;}$trans
-.float-left{float:left;}$trans
-.float-right{float:right;}$trans
+.float-left{float:left !important;}$trans
+.float-right{float:right !important;}$trans
 .full-width{width:100% !important }$trans
-.fly-out{position: absolute;left: -10000px;}$trans
-.text-center{text-align:center;}$trans
-.text-right{text-align:right;}$trans
-.text-left{text-align:left }$trans
-.pointer{cursor:pointer;}$trans 
-.hidden{visibility: hidden;}$trans
-.clear{clear:both;}$trans
-.clearfix{overflow:hidden;}$trans
-.position-absolute{position:absolute }$trans
-.position-relative{position:relative }$trans
-.bold{font-weight:600}$trans
-.p-0{padding:0}$trans
-.p-r0,.px-0{padding-right:0}$trans
-.pl-0,.px-0{padding-left:0}$trans
-.pt-0,.py-0{padding-top:0}$trans
-.pb-0,py-0{padding-bottom:0}$trans
-.m-0{margin:0;}$trans
-.mx-auto{margin-right:auto;margin-left:auto;}$trans
-.mr-0,.mx-0{margin-right:0}$trans
-.ml-0,.mx-0{margin-left:0}$trans
-.mt-0,.my-0{margin-top:0}$trans
-.mb-0,.my-0{margin-bottom:0}$trans
-.border0{border:0;}$trans
-.t-0{top:0;}$trans
-.r-0{right:0}$trans
-.l-0{left:0}$trans
-.b-0{bottom:0}$trans
-.border-r0{border-right:0}$trans
-.border-l0{border-left:0}$trans
-.border-t0{border-top:0}$trans
-.border-b0{border-bottom:0}$trans
-.left0{left:0;}$trans
-.right0{right:0;}$trans
+.fly-out{position: absolute;left: -10000px !important;}$trans
+.text-center{text-align:center !important;}$trans
+.text-right{text-align:right !important;}$trans
+.text-left{text-align:left !important}$trans
+.pointer{cursor:pointer !important;}$trans 
+.hidden{visibility: hidden !important;}$trans
+.clear{clear:both !important;}$trans
+.clearfix{overflow:hidden !important;}$trans
+.position-absolute{position:absolute !important}$trans
+.position-relative{position:relative !important}$trans
+.bold{font-weight:600 !important}$trans
+.p-0{padding:0 !important}$trans
+.p-r0,.px-0{padding-right:0 !important}$trans
+.pl-0,.px-0{padding-left:0 !important}$trans
+.pt-0,.py-0{padding-top:0 !important}$trans
+.pb-0,py-0{padding-bottom:0 !important}$trans
+.m-0{margin:0 !important;}$trans
+.mx-auto{margin-right:auto !important;margin-left:auto !important;}$trans
+.mr-0,.mx-0{margin-right:0 !important}$trans
+.ml-0,.mx-0{margin-left:0 !important}$trans
+.mt-0,.my-0{margin-top:0 !important}$trans
+.mb-0,.my-0{margin-bottom:0 !important}$trans
+.border0{border:0 !important;}$trans
+.t-0{top:0 !important;}$trans
+.r-0{right:0 !important}$trans
+.l-0{left:0 !important}$trans
+.b-0{bottom:0 !important}$trans
+.border-r0{border-right:0 !important}$trans
+.border-l0{border-left:0 !important}$trans
+.border-t0{border-top:0 !important}$trans
+.border-b0{border-bottom:0 !important}$trans
+.left0{left:0 !important;}$trans
+.right0{right:0 !important;}$trans
 .bottom0{bottom:0;}$trans
-.top0{top:0;}$trans
-.index-9{z-index:9}$trans
-.index-99{z-index:99}$trans
-.index-999{z-index:999}$trans
-.index-9999{z-index:9999}$trans
-.w-25{width:25%}$trans
-.w-50{width:50%}$trans
-.w-75{width:75%}$trans
-.w-100{width:100%}$trans
-.h-100{height: 100%;}$trans
-.line-height75{line-height: 75%;}$trans
-.line-height100{line-height: 100%;}$trans
-.lights:hover{color:#666;}$trans
+.top0{top:0 !important;}$trans
+.index-9{z-index:9 !important}$trans
+.index-99{z-index:99 !important}$trans
+.index-999{z-index:999 !important}$trans
+.index-9999{z-index:9999 !important}$trans
+.w-25{width:25% !important}$trans
+.w-50{width:50% !important}$trans
+.w-75{width:75% !important}$trans
+.w-100{width:100% !important}$trans
+.h-100{height: 100% !important;}$trans
+.line-height75{line-height: 75% !important;}$trans
+.line-height100{line-height: 100% !important;}$trans
+.lights:hover{color:#666 !important;}$trans
 ";
 echo "/*end*/\r\n"; 
 if($scene=='web'){
 echo <<<ET
-.page{max-width: 750px;min-width:320px;margin: 0 auto;}$trans
+.page{max-width: 750px !important;min-width:320px !important;margin: 0 auto !important;}$trans
 
 ET;
 }
@@ -156,29 +156,29 @@ echo "/*枚举部分-字号*/$trans";
 $font_s=8;
 $font_e=20;
 for ($i=$font_s;$i<=$font_e;$i=$i+2) {
-	echo ".font".$i."{font-size:".$i*$times."px;}$trans";
+	echo ".font".$i."{font-size:".$i*$times."px !important;}$trans";
 }
 echo "/*枚举部分-外边距*/$trans";  
 $s=5;
 $step=5;
 $e=100;
 for($i=$s;$i<=$e;$i=$i+$step){
-	echo ".m-".$i."{margin:".$i*$times.$extra.";}$trans";
-	echo ".mt-".$i.",.my-".$i."{margin-top:".$i*$times.$extra.";}$trans";
-	echo ".mr-".$i.",.mx-".$i."{margin-right:".$i*$times.$extra.";}$trans";
-	echo ".mb-".$i.",.my-".$i."{margin-bottom:".$i*$times.$extra.";}$trans";
-	echo ".ml-".$i.",.mx-".$i."{margin-left:".$i*$times.$extra.";}$trans";   
+	echo ".m-".$i."{margin:".$i*$times.$extra." !important;}$trans";
+	echo ".mt-".$i.",.my-".$i."{margin-top:".$i*$times.$extra." !important;}$trans";
+	echo ".mr-".$i.",.mx-".$i."{margin-right:".$i*$times.$extra." !important;}$trans";
+	echo ".mb-".$i.",.my-".$i."{margin-bottom:".$i*$times.$extra." !important;}$trans";
+	echo ".ml-".$i.",.mx-".$i."{margin-left:".$i*$times.$extra." !important;}$trans";   
 }
 echo "/*枚举部分-内边距*/$trans";  
 $s=5;
 $step=5;
 $e=100;
 for($i=$s;$i<=$e;$i=$i+$step){
-	echo ".p-".$i."{padding:".$i*$times.$extra.";}$trans";
-	echo ".pt-".$i.",.py-".$i."{padding-top:".$i*$times.$extra.";}$trans";
-	echo ".pr-".$i.",.px-".$i."{padding-right:".$i*$times.$extra.";}$trans";
-	echo ".pb-".$i.",.py-".$i."{padding-bottom:".$i*$times.$extra.";}$trans";
-	echo ".pl-".$i.",.px-".$i."{padding-left:".$i*$times.$extra.";}$trans";   	
+	echo ".p-".$i."{padding:".$i*$times.$extra." !important;}$trans";
+	echo ".pt-".$i.",.py-".$i."{padding-top:".$i*$times.$extra." !important;}$trans";
+	echo ".pr-".$i.",.px-".$i."{padding-right:".$i*$times.$extra." !important;}$trans";
+	echo ".pb-".$i.",.py-".$i."{padding-bottom:".$i*$times.$extra." !important;}$trans";
+	echo ".pl-".$i.",.px-".$i."{padding-left:".$i*$times.$extra." !important;}$trans";   	
 } 
 }
 
